4353b2d5d51a38f9f48a977f6e5b3ebd4fde8ba6
Subham-shaw <ytrakhishaw@gmail.com>
2022-03-22 18:02:03 +0530

    Some Changes

--- handlers/logger.js
+++ handlers/logger.js
@@ -404,7 +404,7 @@ async function send_log(c, guild, color, title, description, thumb, fieldt, fiel
       .setThumbnail(thumb ? thumb : guild?.iconURL({
         format: "png"
       }))
-      .setFooter(c.getFooter(guild?.name + "PythonNodes | https://pythonnodes.xyz", guild?.iconURL({
+      .setFooter(c.getFooter(guild?.name + "Hostila Nodes | https://hostilanodes.xyz/", guild?.iconURL({
         format: "png"
       })))
     if(fieldt && fieldv){
